9bbc8c959edc9eaab03f846f0016cd669eca45f8
shockrah <dev@shockrah.xyz>
2024-09-26 20:18:40 -0700

    Using -p to copy file state i think

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [[ "$INPUT_KEY" ]]; then
     chmod 400 key                  # Set the key as Read-Only
     echo "Trying SCP process with SSH key"
     # Runs the SCP command
-    scp -r -P $INPUT_PORT -o StrictHostKeyChecking=no -i key $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
+    scp -rp -P $INPUT_PORT -o StrictHostKeyChecking=no -i key $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
 else
     # If the keyis empty, it uses the password for the SCP
     echo "Trying SCP process with password"
