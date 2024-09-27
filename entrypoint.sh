5ff3f80e6f48fc5f85fd9b0f85602d3b61e05e28
shockrah <dev@shockrah.xyz>
2024-09-26 23:49:57 -0700

    Adding archive flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [[ "$INPUT_KEY" ]]; then
     echo -e "${INPUT_KEY}" > key   # Creates a file with the key content
     chmod 400 key                  # Set the key as Read-Only
 
-	rsync -v --stats -e "ssh -p $INPUT_PORT -o StrictHostKeyChecking=no -i key" \
+	rsync -a -v --stats -e "ssh -p $INPUT_PORT -o StrictHostKeyChecking=no -i key" \
 		"$INPUT_ORIGIN" \
 		"$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
 fi
