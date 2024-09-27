a10783e281e968d8138ce4a5543d8b090b79c466
shockrah <dev@shockrah.xyz>
2024-09-26 20:35:07 -0700

    Switchign to rsync

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,15 @@
 
 # Checking if the key input is not empty
 if [[ "$INPUT_KEY" ]]; then
-    # If it is not empty, it uses the key for the SCP
+    # If it is not empty, it uses the key for the rsync command
     echo -e "${INPUT_KEY}" > key   # Creates a file with the key content
     chmod 400 key                  # Set the key as Read-Only
-    echo "Trying SCP process with SSH key"
+
+	rsync -e "ssh -p $INPUT_PORT -o StrictHostKeyChecking=no -i key" \
+		"$INPUT_ORIGIN" \
+		"$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
     # Runs the SCP command
-    scp -rp -P $INPUT_PORT -o StrictHostKeyChecking=no -i key $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
-else
-    # If the keyis empty, it uses the password for the SCP
-    echo "Trying SCP process with password"
-    # Runs the SCP command
-    sshpass -r -p $INPUT_PASSWORD scp -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
+    scp -i key $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
 fi
 time=$(date)
 echo "-----------------------------"
