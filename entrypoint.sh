731c33a2e95ab11b77ccdfde852ca151741bcb20
shockrah <dev@shockrah.xyz>
2024-09-28 14:56:34 -0700

    removing bash style checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,6 @@
 
 set -e
 
-# Quick checks for missing parameters
-empty() {
-	echo "ERROR: $1 is empty"
-}
-
-[[ "$INPUT_HOST" ]]   		&& empty host
-[[ "$INPUT_SOURCE" ]] 		&& empty source
-[[ "$INPUT_DESTINATION" ]] 	&& empty destination
-[[ "$INPUT_USERNAME" ]] 		&& empty username
-[[ "$INPUT_KEY" ]] 			&& empty key
-
-
-
 echo -e "${INPUT_KEY}" > key
 chmod 400 key
 
