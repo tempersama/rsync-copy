144fd456bace5face28b46fd37ddf1308549273c
shockrah <dev@shockrah.xyz>
2024-09-28 15:53:20 -0700

    Omit directory time setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ chmod 400 key
 
 # TODO: make sure we are not just blindly using StrictHostKeyChecking=no
 rsync \
-	-a -v --stats \
+	-a -v -O --stats \
 	-e "ssh -o StrictHostKeyChecking=no -i key" \
 	--port "$INPUT_PORT" \
 	"$INPUT_SOURCE" \
